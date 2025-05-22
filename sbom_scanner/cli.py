1ca6a3717fffed0e36285a9342329b05fb01bf79
Jordan Croft <jcroft03@qub.ac.uk>
2025-05-22 11:19:02 +0100

    finished scanner file and began format in cli file

---
+++ sbom_scanner/cli.py
@@ -0,0 +1,45 @@
+import argparse
+from sbom_scanner import scanner
+from rich.console import Console
+from rich.table import Table
+
+
+
+def format_results(filepath):
+    results = scanner.scan_sbom(filepath)
+    console = Console()
+
+    if not results:
+        console.print("[green]✅ No known vulnerabilities found in the SBOM.[/green]")
+        return
+
+    table = Table(title="🔍 CVE Scan Results from SBOM")
+
+    table.add_column("Package", style="cyan", no_wrap=True)
+    table.add_column("Version", style="magenta")
+    table.add_column("CVE(s)", style="red")
+    table.add_column("Summary", style="white")
+    table.add_column("Severity", style="yellow")
+
+    for vuln in results:
+        aliases = " / ".join(vuln["cve_id"]) if vuln["cve_id"] else "N/A"
+        summary = vuln["summary"][:100] + "..." if len(vuln["summary"]) > 100 else vuln["summary"]
+
+        if vuln["severity"]:
+            sev = ", ".join([s.get("score", "N/A") for s in vuln["severity"]])
+        else:
+            sev = "N/A"
+
+        table.add_row(
+            vuln["package"],
+            vuln["version"],
+            aliases,
+            summary,
+            sev
+        )
+
+    console.print(table)
+
+
+if __name__ == "__main__":
+    format_results("/Users/jordancroft/Documents/Documents - Jordan.’s MacBook Air/GitHub/sbom-vuln-scan/sample_data/sample_sbom.json")
--- sbom_scanner/scanner.py
+++ sbom_scanner/scanner.py
@@ -1,1 +1,43 @@
-from . import cli
+from sbom_scanner.sbom_parser import parse_sbom
+from sbom_scanner.osv_api import query_osv
+
+def scan_sbom(filepath, ecosystem="PyPI"):
+    """
+
+    :param filepath:
+    :param ecosystem:
+    :return:
+    """
+    results = []
+
+    # packages will now contain list of tuples [(name, verion)]
+    packages = parse_sbom(filepath)
+
+    # loop through each package and query OSV
+    for name, version in packages:
+        # return vulns for that package
+        vulns = query_osv(name, version)
+        if not vulns:
+            continue
+
+        # filter for only the most recent vulnerability
+        top_vuln = vulns[0]
+
+
+        aliases = top_vuln.get("aliases", [top_vuln.get("id", "N/A")])
+        summary = top_vuln.get("summary", "No description")
+        severity = top_vuln.get("severity", [])
+
+        # add this to results as a dict
+        results.append({
+            "package": name,
+            "version": version,
+            "cve_id": aliases,
+            "summary": summary,
+            "severity": severity
+        })
+    return results
+
+
+if __name__ == "__main__":
+    print(scan_sbom("/Users/jordancroft/Documents/Documents - Jordan.’s MacBook Air/GitHub/sbom-vuln-scan/sample_data/sample_sbom.json"))
